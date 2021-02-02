150da2fd36b9fc23c804d1f4898f9dc3fb1f5abe
Kamen Yovchev <kamen.yovchev@wize-soluions.com>
2021-02-02 15:32:31 +0200

    add changes to the name

--- key_vault.tf
+++ key_vault.tf
@@ -13,7 +13,6 @@ resource "azurerm_key_vault" "key_vault" {
   soft_delete_enabled             = var.soft_delete_enabled
   soft_delete_retention_days      = var.soft_delete_retention_days
   purge_protection_enabled        = var.purge_protection_enabled
-  enable_rbac_authorization       = var.enable_rbac_authorization
   sku_name = var.sku_name
 
   access_policy {
